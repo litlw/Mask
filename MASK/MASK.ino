ab12c38b969e7ecf47fce400212ee294c91c8027
litlw <hasaunderritchie@gmail.com>
2016-01-19 12:14:36 -0500

    Opening File
    This is the first update.

---
+++ MASK/MASK.ino
@@ -0,0 +1,14 @@
+/* this is the code for the MASK PROJECT for PHYSICAL COMPUTING
+ *  Collaborators : Hugh Ritchie, Samantha DuuNOT
+ *  Start Date : JAN19 
+
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
