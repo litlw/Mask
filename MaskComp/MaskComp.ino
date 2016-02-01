d44032d19020b2f8e1b200e2866652c1f4a7e9d8
litlw <hasaunderritchie@gmail.com>
2016-02-01 15:48:01 -0500

    updates. confusion?

--- MaskComp/MaskComp.ino
+++ MaskComp/MaskComp.ino
@@ -19,10 +19,12 @@ const int button = 4;
 const int servo = 2;
 int val = 0; //placeholder
 int oldval = LOW; // delay placeholder
-int x = 0;
+int x = 0; //program start
+int audiostart = 0; //start sound
+int servostart = 0; //start servo motor
 // i'll use this placeholder later. this is the key component. the magic stuff.
 int pos = 0; //where the servo is
-int audio = 0;
+
 
 long timer = 0;
 //keeping track of time. not sure if ill use it.
@@ -62,71 +64,55 @@ void loop() {
   val = digitalRead(button);
   if ((val == HIGH) && (oldval == LOW)) {
     x = 1;
-    digitalWrite(led, LOW);
-    audio = 0;
   }
+  oldval = val;
+   myservo.write(pos);
+  
+  if (timer == ret) {
+        x = 0;
+        timer = 0;
+        
+      }
 
   if (x == 1) {
-    for (timer == 0; timer < ret; timer ++) {
+    for (timer == 0; timer <= ret; timer ++) { // the timer begins if the program started. woot. 
       delay(10);
-      if ((timer > go) && (timer < ret)) {
-        audio = 1;
-        if (audio == 1) {
+    }
+  }
+
+//I have seperated the start times of the audio and the machine. I was having problems with the two processes running at the same time. 
+  
+      if ((timer > go) && (timer < ret)) { //makes the program wait some time before the button makes stuff happen 
+       audiostart = 1; // the audio will start if this is true. 
+        if (audiostart == 1) {
           result = MP3player.playTrack(1);
           delay (220);
-          digitalWrite(led, HIGH);
-          x = 0;
-          
         }
-       if (timer == talk) {
+      }
+        
+       if ((timer > talk)&&(timer < ret)) { //makes the program wait some time before the button makes the mouth move. worth noting, the numbers are different for a reason.
+        servostart = 1; // the mouth will move open
+        if (servostart == 1){
             pos = 100;
-            delay (30);
-            audio = 0;
+          } else {
+            pos = 0; // the mouth will close if the whatever is = 0
           }
       }else {
-          //pos = 0;
+          servostart = 0;
         }
-      }
-      } else {}
-      if (timer == ret) {
-        MP3player.stopTrack();
-        x = 0;
-        val = LOW;
-        pos = 0;
-        timer = 0;
-      }
-      myservo.write(pos);
-    oldval = val;
+
+        if (x==0){
+         MP3player.stopTrack();
+
+        }
+
+
+        
+      
+     
+      
+     
+
     
     }
-
-
-  //oldval = val;
-  /*
-
-
-    if ((timer < ret) && (x == 1)) {
-    timer ++;
-    delay(10);
-    if (timer > go) {
-    result = MP3player.playTrack(1);
-    digitalWrite(led, HIGH);
-    pos = 0;
-    } else if ((timer > go) && (timer > talk)) {
-
-    pos = 100;
-    } else {
-    pos = 0;
-    }
-    } else {}
-
-    if ((timer >= ret) && (x == 1)) {
-    timer = 0;
-    x = 0;
-    MP3player.stopTrack();
-    digitalWrite (led, LOW);
-    }
-
-    myservo.write(pos);  // must be at the end
-  */
 
