35628c2470007219d2ddd32e17e09f0ff2239810
litlw <hasaunderritchie@gmail.com>
2016-02-02 17:50:16 -0500

    Changes for final
    Final changes to suit the final products. will still work on it

--- MaskComp/MaskComp.ino
+++ MaskComp/MaskComp.ino
@@ -14,11 +14,11 @@ SdFile file;
 
 Servo myservo; //this is my servo
 
-const int led = 10;
-const int button = 4;
-const int servo = 2;
+const int led = 10; // these are gonna light up the box
+const int led2 = 3; // this will be the "eyes" of the thing
+const int button = 4; // the obvious button
+const int servo = 2; // rotates the head. 
 int val = 0; //placeholder
-int oldval = LOW; // delay placeholder
 int x = 0; //program start
 int audiostart = 0; //start sound
 int servostart = 0; //start servo motor
@@ -26,8 +26,7 @@ int servostart = 0; //start servo motor
 int pos = 0; //where the servo is
 
 
-long timer = 0;
-//keeping track of time. not sure if ill use it.
+long timer = 0;//keeping track of time. not sure if ill use it.
 const long ret = 310;    //difference between ret and go is duration the mouth will be open
 const long go = 100;
 const long talk = 300;
@@ -42,13 +41,15 @@ byte temp;
 void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
   pinMode(button, INPUT);
+  result = sd.begin(SD_SEL, SPI_HALF_SPEED);
+   result = MP3player.begin();
   myservo.attach(servo);
   Serial.begin(9600); // unsure why the serial is this high
-  result = sd.begin(SD_SEL, SPI_HALF_SPEED);
   //tells the shield to boot up
 
-  result = MP3player.begin();
+ 
   //tells the sheild it's a mp3 player now.
 
 }
@@ -62,57 +63,39 @@ void setup() {
 
 void loop() {
   val = digitalRead(button);
-  if ((val == HIGH) && (oldval == LOW)) {
+  
+ if (timer == ret) {
+    x = 0;
+    timer = 0;
+    pos = 0;
+ }
+  if ((val == HIGH) && (timer == 0)) {
     x = 1;
   }
-  oldval = val;
-   myservo.write(pos);
   
-  if (timer == ret) {
-        x = 0;
-        timer = 0;
-        
-      }
+  myservo.write(pos);
+
+  
 
   if (x == 1) {
-    for (timer == 0; timer <= ret; timer ++) { // the timer begins if the program started. woot. 
+    timer ++;  // the timer begins if the program started. woot.
       delay(10);
-    }
   }
 
-//I have seperated the start times of the audio and the machine. I was having problems with the two processes running at the same time. 
-  
-      if ((timer > go) && (timer < ret)) { //makes the program wait some time before the button makes stuff happen 
-       audiostart = 1; // the audio will start if this is true. 
-        if (audiostart == 1) {
-          result = MP3player.playTrack(1);
-          delay (220);
-        }
-      }
-        
-       if ((timer > talk)&&(timer < ret)) { //makes the program wait some time before the button makes the mouth move. worth noting, the numbers are different for a reason.
-        servostart = 1; // the mouth will move open
-        if (servostart == 1){
-            pos = 100;
-          } else {
-            pos = 0; // the mouth will close if the whatever is = 0
-          }
-      }else {
-          servostart = 0;
-        }
+  //I have seperated the start times of the audio and the machine. I was having problems with the two processes running at the same time.
 
-        if (x==0){
-         MP3player.stopTrack();
-
-        }
+  if (timer > go)  { //I took out the second half of the IF statement, since i solved it earlier in the program. 
+   // audiostart = 1; // the audio will start if this is true.
+   analogWrite(led2, 100);
+         result = MP3player.playTrack(1);
+          delay (40);
+          MP3player.stopTrack();
+  }
 
 
-        
-      
-     
-      
-     
-
-    
-    }
+  if (timer > talk) { //makes the program wait some time before the button makes the mouth move. worth noting, the numbers are different for a reason.
+   // servostart = 1; // the mouth will move open
+      pos = 100;
+    } else {}
+}
 
